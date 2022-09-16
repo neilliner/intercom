8c13d4af2f321da29f555dc41389e2bbd660f1da
Neil <epoch148@hotmail.com>
2022-09-16 19:40:07 +0700

    [0] Fix build

--- android/src/main/java/com/getcapacitor/community/intercom/IntercomPlugin.java
+++ android/src/main/java/com/getcapacitor/community/intercom/IntercomPlugin.java
@@ -1,6 +1,6 @@
 package com.getcapacitor.community.intercom;
 
-import com.getcapacitor.Config;
+// import com.getcapacitor.Config;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
